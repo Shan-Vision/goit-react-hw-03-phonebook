c74bbd7b1fd9628ed188a5e2e69f8a280a36eef7
Sherzod Abdullaev <perfekt.vision2019@gmail.com>
2022-07-09 17:48:39 +0200

    used localStorage and lifeCircle methods of a class

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,13 +6,22 @@ import shortid from 'shortid';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
+  };
+
+  componentDidMount = () => {
+    const contactsFromLocalStorage = localStorage.getItem('contacts');
+    const parsedStorageContacts = JSON.parse(contactsFromLocalStorage);
+    if (parsedStorageContacts) {
+      this.setState({ contacts: parsedStorageContacts });
+    }
+  };
+
+  componentDidUpdate = (prevProps, prevState) => {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
   };
 
   handleSubmitForm = ({ contact }) => {
@@ -43,7 +52,7 @@ class App extends Component {
   };
   render() {
     const contactNameList = this.state.contacts.map(contact => contact.name);
-    
+
     return (
       <div
         style={{
